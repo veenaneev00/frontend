857255aac5c4ae842be0840ceed5aa537e3375ef
veenaneev00 <veenaneev00@gmail.com>
2025-11-22 22:07:08 +0530

    updated components and pages

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M3 3h7v7H3V3zm0 11h7v7H3v-7zm11-11h7v7h-7V3zm0 11h7v7h-7v-7z" fill="currentColor"/>
             </svg>
-            <span>Skitbit</span>
+            <span>RenderDac</span>
           </Link>
 
           {/* Desktop Navigation */}
