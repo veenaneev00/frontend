ff800dffbb62cd94d86a879a8bc6068c30cc7cb6
veenaneev00 <veenaneev00@gmail.com>
2025-12-25 02:13:58 +0530

    ok

--- src/componets/Footer.tsx
+++ src/componets/Footer.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../assets/css/sections/footer.css";
 import { Phone, Mail, MapPin } from "lucide-react";
